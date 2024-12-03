09511b3fb32c0950a70446d4bc653e53baf22c4f
Jacob P. <jjpfst@gmail.com>
2024-12-03 16:25:39 +0100

    Pfad zum Template angepasst

--- frontend/rptu4you/news/views.py
+++ frontend/rptu4you/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def Latest_News(request):
-    return render(request, 'LatestNews.html')
+    return render(request, "templates/news/Latest_News.html")
