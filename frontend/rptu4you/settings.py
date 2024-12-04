873a2e5e26abf9ad7fa434d642b4b9c1127475ef
Jacob P. <jjpfst@gmail.com>
2024-12-04 04:38:16 +0100

    Da hat noch der Projekt-Ordner gefehlt

--- frontend/rptu4you/settings.py
+++ frontend/rptu4you/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+
 import os
 from pathlib import Path
 
@@ -64,8 +65,8 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, 'news/templates/news'), 
-            os.path.join(BASE_DIR, 'templates'),  
+            os.path.join(BASE_DIR, "rptu4you/news/templates/news"),
+            os.path.join(BASE_DIR, "templates"),
         ],
         "APP_DIRS": True,
         "OPTIONS": {
