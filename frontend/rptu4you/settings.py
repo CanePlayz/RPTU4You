1176ee8bc811d3c6e833fdbb9341e6fe3cde253c
Jacob P. <jjpfst@gmail.com>
2024-12-04 22:07:21 +0100

    Anbindung von Django an Datenbank

--- frontend/rptu4you/settings.py
+++ frontend/rptu4you/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = "rptu4you.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydb",
+        "USER": "admin",
+        "PASSWORD": "password",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
