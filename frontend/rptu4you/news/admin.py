dfbd5b202ad9f00033d08f0bfe27845e1134b5ed
Jacob P. <jjpfst@gmail.com>
2025-03-18 16:27:57 +0100

    Admin-Interface erweitert
    Co-authored-by: Hummel132 <Hummel132@users.noreply.github.com>

--- frontend/rptu4you/news/admin.py
+++ frontend/rptu4you/news/admin.py
@@ -1,7 +1,13 @@
+import models
 from django.contrib import admin
 
-from .models import Fachschaft, News, User
+admin.site.register(models.Quelle)
+admin.site.register(models.Fachschaft)
+admin.site.register(models.Rundmail)
+admin.site.register(models.InterneWebsite)
+admin.site.register(models.ExterneWebsite)
+admin.site.register(models.Standort)
+admin.site.register(models.Kategorie)
+admin.site.register(models.News)
 
-admin.site.register(User)
-admin.site.register(Fachschaft)
-admin.site.register(News)
+admin.site.register(models.User)
