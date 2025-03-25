bec5977246db8b3159698aa42e1edebec1217b78
Jacob P. <jjpfst@gmail.com>
2025-03-25 21:48:49 +0100

    Superuser-Erstellung in Docker-Compose-Befehlen hinzugefügt

--- frontend/create_superuser.py
+++ frontend/create_superuser.py
@@ -1,5 +1,10 @@
 import os
 
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rptu4you.settings")
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
