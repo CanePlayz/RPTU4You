ce9dd0f55befd8ef5ecd337c7eae0a8a6096e5e9
Jacob P. <jjpfst@gmail.com>
2025-06-27 08:41:50 +0200

    Background-Task zur nachträglichen Kategorisierung hinzugefügt

--- frontend/rptu4you/news/tasks.py
+++ frontend/rptu4you/news/tasks.py
@@ -1,8 +1,10 @@
+import os
+
 from bs4 import BeautifulSoup
 from celery import shared_task
 from deep_translator import GoogleTranslator
-from rptu4you.news.models import News, Sprache, Text
 
+from .models import *
 from .views.util.categorization.categorization import get_categorization_from_openai
 from .views.util.translate import translate_html
 
@@ -60,4 +62,27 @@ def backfill_missing_translations():
 
 @shared_task
 def backfill_missing_categorizations():
-    pass
+    environment = os.getenv("ENVIRONMENT", "")
+    openai_api_key = os.getenv("OPENAI_API_KEY", "")
+    news_items = News.objects.all()
+    for news in news_items:
+        if not news.kategorien.exists():
+            print(f"News-Objekt {news.titel} benötigt Kategorisierung.")
+            text_object = Text.objects.filter(news=news, sprache__name="Deutsch")
+            try:
+                german_text = text_object.get().text
+            except Text.DoesNotExist:
+                print(f"Kein deutscher Text für {news.titel} gefunden.")
+                continue
+            categories, audiences = get_categorization_from_openai(
+                news.titel, german_text, environment, openai_api_key
+            )
+            for category in categories:
+                category_object, _ = InhaltsKategorie.objects.get_or_create(
+                    name=category
+                )
+                news.kategorien.add(category_object)
+
+            for audience in audiences:
+                audience_object, _ = Zielgruppe.objects.get_or_create(name=audience)
+                news.zielgruppe.add(audience_object)
